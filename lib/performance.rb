98992000d22efaa62bf58da2402aca382d18c323
shket344 <kalimador@tut.by>
2023-11-13 18:17:14 +0300

    Update common files

--- lib/performance.rb
+++ lib/performance.rb
@@ -3,16 +3,19 @@
 require_relative 'application'
 
 class Performance < Application
-  attr_reader :connection_validator, :reader, :file_path
+  attr_reader :connection_validator, :folder_validator, :folder_path, :reader, :file_path
 
-  def initialize(connection_validator, reader, file_path)
+  def initialize(connection_validator, folder_validator, folder_path, reader, file_path)
     @connection_validator = connection_validator
+    @folder_validator = folder_validator
+    @folder_path = folder_path
     @reader = reader
     @file_path = file_path
   end
 
   def call
     connection_validator.call
+    folder_validator.call(folder_path)
     reader.call(file_path)
   end
 end
